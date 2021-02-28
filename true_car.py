9fe0e127d647a56316ff2097415f11ebe3348b75
Maxwell Bertolero <mbertolero@me.com>
2021-02-28 09:17:19 -0500

    tweak to check for csv

--- true_car.py
+++ true_car.py
@@ -4,29 +4,34 @@ import time
 import numpy as np
 from datetime import datetime
 import pandas as pd
+import seaborn as sns
+import matplotlib.pylab as plt
 
 def create_naked_df():
 	df = pd.DataFrame(columns=['date','trim','truecar','inventory'])
-	df.to_csv('prices&inventory.csv',index=False) 
+	df.to_csv('prices&inventory.csv',index=False)
 
 
-df = pd.read_csv('prices&inventory.csv')
+try: df = pd.read_csv('prices&inventory.csv')
+except: create_naked_df()
 
 sport = 'https://www.truecar.com/prices-new/honda/ridgeline-summary/2021/?postalCode=19118&section=review&styleId=72032'
 rtl = 'https://www.truecar.com/prices-new/honda/ridgeline-summary/2021/?postalCode=19118&section=review&styleId=72033'
 rtl_e = 'https://www.truecar.com/prices-new/honda/ridgeline-summary/2021/?postalCode=19118&section=review&styleId=72035'
 black = 'https://www.truecar.com/prices-new/honda/ridgeline-summary/2021/?postalCode=19118&section=review&styleId=72037'
+sport20 = 'https://www.truecar.com/prices-new/honda/ridgeline-summary/2020/?postalCode=19118&section=review&styleId=67796'
 
 prices = []
-for name,url in zip(['sport','rtl','rtl_e','black'],[sport,rtl,rtl_e,black]):
+for name,url in zip(['sport','rtl','rtl_e','black','2020'],[sport,rtl,rtl_e,black,sport20]):
 	browser = webdriver.Safari()
 	browser.get(url)
 	time.sleep(2)
-	main = browser.find_element_by_id("main") 
+	main = browser.find_element_by_id("main")
 	time.sleep(2)
 	price = main.find_element_by_class_name("col-md-6").text.split('$')
 	if len(price) == 3:
-		price = np.mean([int(price[1].split(' ')[0].replace(',', '')),int(price[2].split(' ')[0].replace(',', ''))])
+		try:price = np.mean([int(price[1].split(' ')[0].replace(',', '')),int(price[2].split(' ')[0].replace(',', ''))])
+		except:price = price[1].split(' ')[0].replace(',', '')
 	elif len(price) ==2:
 		price = price[1].split(' ')[0].replace(',', '')
 	print (price)
@@ -37,9 +42,9 @@ black = "https://www.autotrader.com/cars-for-sale/new-cars/truck/honda/ridgeline
 rtl = 'https://www.autotrader.com/cars-for-sale/new-cars/truck/honda/ridgeline/philadelphia-pa-19118?incremental=ALL&dma=&driveGroup=AWD4WD&searchRadius=100&location=&startYear=2021&trimCodeList=RIDGELINE%7CRTL&isNewSearch=true&marketExtension=include&showAccelerateBanner=false&sortBy=relevance&numRecords=25'
 rtl_e = 'https://www.autotrader.com/cars-for-sale/new-cars/truck/honda/ridgeline/philadelphia-pa-19118?incremental=ALL&dma=&driveGroup=AWD4WD&searchRadius=100&location=&startYear=2021&marketExtension=include&trimCodeList=RIDGELINE%7CRTL-E&isNewSearch=true&showAccelerateBanner=false&sortBy=relevance&numRecords=25'
 sport = 'https://www.autotrader.com/cars-for-sale/new-cars/truck/honda/ridgeline/philadelphia-pa-19118?incremental=ALL&dma=&driveGroup=AWD4WD&searchRadius=100&location=&startYear=2021&marketExtension=include&trimCodeList=RIDGELINE%7CSport&isNewSearch=true&showAccelerateBanner=false&sortBy=relevance&numRecords=25'
-
+all_2020 = 'https://www.autotrader.com/cars-for-sale/new-cars/truck/2020/honda/ridgeline/philadelphia-pa-19118?incremental=ALL&dma=&driveGroup=AWD4WD&searchRadius=100&location=&marketExtension=include&isNewSearch=true&showAccelerateBanner=false&sortBy=relevance&numRecords=25'
 inventories = []
-for name,url in zip(['sport','rtl','rtl_e','black'],[sport,rtl,rtl_e,black]):
+for name,url in zip(['sport','rtl','rtl_e','black','2020'],[sport,rtl,rtl_e,black,all_2020]):
 	browser = webdriver.Safari()
 	browser.get(url)
 	inventory = browser.find_element_by_xpath('//*[@id="mountNode"]/div[1]/div[3]/div/div[2]/div[2]/div[1]/div[1]/div[1]').text.split(' ')[-2]
@@ -47,8 +52,8 @@ for name,url in zip(['sport','rtl','rtl_e','black'],[sport,rtl,rtl_e,black]):
 	inventories.append(inventory)
 	browser.close()
 
-trims = ['sport','rtl','rtl_e','black']
-date = [datetime.now(),datetime.now(),datetime.now(),datetime.now()]
+trims = ['sport','rtl','rtl_e','black','2020']
+date = [datetime.now(),datetime.now(),datetime.now(),datetime.now(),datetime.now()]
 
 this_df = pd.DataFrame(columns=['date','trim','truecar','inventory'])
 this_df['date'] = date
@@ -57,4 +62,21 @@ this_df['inventory'] = inventories
 this_df['trim'] = trims
 
 df = df.append(this_df,ignore_index=True)
-df.to_csv('prices&inventory.csv',index=False) 
+df.to_csv('prices&inventory.csv',index=False)
+
+
+df = pd.read_csv('prices&inventory.csv')
+df.date = pd.DatetimeIndex(df.date)
+df = df.set_index('date')
+df = df.groupby([df.index.day,df.trim]).mean()
+sns.lineplot(x="date", y="truecar",hue="trim",data=df)
+plt.xticks(rotation=90)
+plt.tight_layout()
+plt.savefig('prices.pdf')
+plt.close()
+
+sns.lineplot(x="date", y="inventory",hue="trim",data=df)
+plt.xticks(rotation=90)
+plt.tight_layout()
+plt.savefig('inventory.pdf')
+plt.close()
